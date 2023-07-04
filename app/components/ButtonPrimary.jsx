24827524a85e18888c770e1a4d7e7bddc48fa8a8
Yevhen <onufrii.socnet@gmail.com>
2023-07-04 17:39:31 +0300

    remove ancors

--- app/components/ButtonPrimary.jsx
+++ app/components/ButtonPrimary.jsx
@@ -5,7 +5,7 @@ export default function ButtonPrimary({ href, title, ...props }) {
     <>
       <Link
         {...props}
-        href={href ? href : '#'}
+        // href={href ? href : '#'}
         className="min-w-[100px] h-[34px] px-[20px] py-1 max-phone:px-[10px] bg-primary rounded-[80px] leading-7 whitespace-nowrap transition duration-[400] ease-in-out hover:bg-[#c3b441]"
       >
         {title}
