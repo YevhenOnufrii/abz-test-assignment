cd7c4a3c78d2576f89d9c95bb48de63e5a67fa58
Yevhen <onufrii.socnet@gmail.com>
2023-07-05 12:55:54 +0300

    edit next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'frontend-test-assignment-api.abz.agency',
-        port: '',
-        pathname: '/images/**',
-      },
-    ],
-  },
+  // output: 'export',
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'frontend-test-assignment-api.abz.agency',
+  //       port: '',
+  //       pathname: '/images/**',
+  //     },
+  //   ],
+  // },
+  images: { unoptimized: true },
 }
 module.exports = nextConfig
